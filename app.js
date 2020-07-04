76fb18b24d4f6a2ee858b60f88ecdb3d6c35d6ae
Phillip Vasil Phillipov <kungy1975@gmail.com>
2020-07-04 03:33:08 -0500

    Added logic to app.js that checks game board for winner or draw.

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 //what are best practices for module pattern, mvc, organising of logic?
 //needed to create board, and board state
 const board = (function () {
-  const boardArr = ["", "", "", "", "", "", "", "", ""];
+  const boardArr = [];
+  const boardRowIndexes = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  //returns string of board row vals
+  const getRowVals = (ind1, ind2, ind3) => {
+    return `${boardArr[ind1]}${boardArr[ind2]}${boardArr[ind3]}`;
+  };
+
+  //check for winning pattern on board
+  const _checkForWin = () => {
+    const rowMarks = boardRowIndexes.map((row) => getRowVals(...row));
+
+    for (let i = 0; i < rowMarks.length; i++) {
+      const marks = rowMarks[i];
+      if (marks === "XXX") {
+        control.declareWinner(player1.getName(), i);
+      } else if (marks === "OOO") {
+        control.declareWinner(player2.getName(), i);
+      } else if (boardArr.length === 9 && !boardArr.includes(undefined)) {
+        control.declareDraw();
+      }
+    }
+    console.log(boardArr, boardArr.length, boardArr[1]);
+  };
 
   //add mark to board at specified index
   const addToBoardArr = (index, isPlayer1Turn) => {
-    const mark = isPlayer1Turn ? player1.mark : player2.mark;
+    const mark = isPlayer1Turn ? player1.getMark() : player2.getMark();
 
     if (!boardArr[index]) {
       boardArr[index] = mark;
       control.renderMarks(boardArr);
-      checkForWin(boardArr);
+      _checkForWin();
     }
   };
 
@@ -29,6 +61,14 @@ const control = (function () {
     });
   };
 
+  const declareWinner = (name) => {
+    console.log(name + " is the winner");
+  };
+
+  const declareDraw = () => {
+    console.log("Games a draw");
+  };
+
   //eventlisteners
   document.querySelector("#board-grid").addEventListener("click", (e) => {
     const el = e.target;
@@ -39,7 +79,7 @@ const control = (function () {
       _isPlayer1Turn = _isPlayer1Turn ? false : true;
     }
   });
-  return { renderMarks };
+  return { renderMarks, declareWinner, declareDraw };
 })();
 
 //needed to build players
@@ -56,6 +96,10 @@ const playerFactory = (name, mark) => {
     return _playerName;
   };
 
+  const getMark = () => {
+    return mark;
+  };
+
   const addToScore = () => {
     _playerScore++;
   };
@@ -64,7 +108,7 @@ const playerFactory = (name, mark) => {
     return _playerScore;
   };
 
-  return { changeName, getName, addToScore, getScore, mark };
+  return { changeName, getName, addToScore, getScore, getMark };
 };
 
 const player1 = playerFactory("X", "X");
